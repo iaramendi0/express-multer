4abf7c22eaf754c1b4742c544ae57e1bdac4c1f6
iaramendi0 <144702548+iaramendi0@users.noreply.github.com>
2023-12-11 15:47:58 +0000

    aa

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,24 @@
 var express = require('express');
 var router = express.Router();
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null,'public/uploads');
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      const fileExtension = file.originalname.split('.').pop();
+      cb(null, file.fieldname + '-' + uniqueSuffix+'.'+fileExtension);
+    },
+  })
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 2 * 1024 * 1024, 
+      },
+  });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,7 +28,7 @@ router.get('/', function(req, res, next) {
 router.post('/', upload.single('avatar'), function (req, res, next) {
     console.log(req.file)
     // req.body will hold the text fields, if there were any
-    res.send("Jasota")
+    res.send(`Zure izena: ${req.body.izena}  URL-a: <a href='https://ominous-waddle-979459p95gw9cxx5w-3000.app.github.dev/uploads/${req.file.filename}'>${req.file.filename}</a>`);
 })
 
 
